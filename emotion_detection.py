66b3c0170d038c5792dc8a553660e8ca188190b6
Christian Michael Kessler <148792985+chrismich-k@users.noreply.github.com>
2026-02-26 06:20:42 -0500

    fix: change API endpoint and headers to emotion detection

--- emotion_detection.py
+++ emotion_detection.py
@@ -31,9 +31,9 @@ def emotion_detector(text_to_analyse):
     response_text : str
         the text attribute of the API response object
     """
-    url = ('https://sn-watson-sentiment-bert.labs.skills.network'
-           '/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict')
-    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    url = ('https://sn-watson-emotion.labs.skills.network'
+           '/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     try:
         response = requests.post(url, json = myobj, headers=headers, timeout=30)
