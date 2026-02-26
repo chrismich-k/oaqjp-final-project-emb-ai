b55c157ef4e7ed80729378f13f262abcf2523bf2
Christian Michael Kessler <148792985+chrismich-k@users.noreply.github.com>
2026-02-26 07:32:13 -0500

    refactor: adapt and simplify exception handling

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,14 +1,19 @@
 """Emotion Detection via IBM watson"""
+
 import requests
+
 
 class EmotionDetectionError(Exception):
     """Base Class for Errors in this Module."""
 
-class EmotionDetectionRequestError(EmotionDetectionError):
-    """Base Class for Errors in this Module."""
 
-class EmotionDetectionFailureError(EmotionDetectionError):
-    """Error class for bad response status codes."""
+class EmotionDetectionInferenceError(EmotionDetectionError):
+    """Input is not suitable to be processed by EmotionDetection (empty or not recognisable)."""
+
+
+class EmotionDetectionTechnicalError(EmotionDetectionError):
+    """EmotionDetection API is not reachable or fails."""
+
 
 def emotion_detector(text_to_analyse):
     """
@@ -18,45 +23,43 @@ def emotion_detector(text_to_analyse):
     ----------
     text_to_analyse : str
         The text that should be analysed
-    
+
     Raises
     ------
-    EmotionDetectionRequestError
-        HTTP request to watson API went wrong
-    EmotionDetectionFailureError
-        watson API couldn't process the text
+    EmotionDetectionError
+        EmotionDetection API is not reachable or fails
+    EmotionDetectionError
+        Input not suitable to be processed by EmotionDetection (empty or not recognisable)
 
     Returns
     -------
     response_text : str
         the text attribute of the API response object
     """
-    url = ('https://sn-watson-emotion.labs.skills.network'
-           '/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
+    url = (
+        "https://sn-watson-emotion.labs.skills.network"
+        "/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyse } }
-    try:
-        response = requests.post(url, json = myobj, headers=headers, timeout=30)
+    myobj = {"raw_document": {"text": text_to_analyse}}
 
-        if response.status_code == 500:
+    # check input
+    if not text_to_analyse or not text_to_analyse.strip():
+        raise EmotionDetectionInferenceError("input is empty or blank.")
+
+    try:
+        response = requests.post(url, json=myobj, headers=headers, timeout=30)
+
+        if response.status_code == 400:
             # watson API couldn't process input string
-            raise EmotionDetectionFailureError(
+            raise EmotionDetectionInferenceError(
                 f"Bad HTTP status code from watson API: {response.status_code}"
             )
 
-        if response.status_code != 200:
-            # raise any other bad http status
-            response.raise_for_status()
-
+        response.raise_for_status()
         return response.text
 
-    except requests.Timeout as e:
-        raise EmotionDetectionRequestError("Request timed out") from e
-    except requests.ConnectionError as e:
-        raise EmotionDetectionRequestError("Error connectiong to watson API") from e
-    except requests.TooManyRedirects as e:
-        raise EmotionDetectionRequestError("Too many redirects for watson API") from e
-    except requests.HTTPError as e:
-        raise EmotionDetectionRequestError("HTTP error for watson API") from e
-    except Exception as e:
-        raise EmotionDetectionRequestError("Unexpected Error when querying for watson API") from e
+    except requests.exceptions.RequestException as e:
+        raise EmotionDetectionTechnicalError(
+            f"Technical API failure: {type(e).__name__}"
+        ) from e
